57dc0dc98bfab307e5ce307af8464bcb58452df6
imansai <im.saibari@gmail.com>
2024-01-04 10:23:48 -0800

    Create a module that uses Pascal's triangle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''A module for using Pascal's triangle.
+'''
+
+
+def pascal_triangle(n):
+    '''Creates an integers lists list representing
+    Pascal's triangle for a specified integer.
+    '''
+    triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
+    return triangle
